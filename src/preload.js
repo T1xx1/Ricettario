feb67be1da0794e14f9c54333a35adc6b74c5df8
T1xx1 <69322987+T1xx1@users.noreply.github.com>
2022-06-05 23:03:03 +0200

    -

--- src/preload.js
+++ src/preload.js
@@ -3,23 +3,20 @@ const { join } = require('path');
 const { readFileSync, writeFileSync } = require('fs');
 
 let path = {
-   database: './src/database',
+   recipes: join(__dirname, 'database/recipes/main.json'),
 };
-
-path['recipes'] = join(path.database, 'recipes.json');
-path['categories'] = join(path.database, 'categories.json');
 
 contextBridge.exposeInMainWorld('preload', {
    recipes: {
-      add: recipe => {
+      add: (name, recipe) => {
          let recipes = JSON.parse(readFileSync(path.recipes, 'utf8'));
 
-         recipes.push(recipe);
+         recipes[name] = recipe;
 
          writeFileSync(path.recipes, JSON.stringify(recipes, null, 3), 'utf8');
       },
-      array: JSON.parse(readFileSync(path.recipes, 'utf8')),
-      categories: JSON.parse(readFileSync(path.categories, 'utf8')),
+      obj: JSON.parse(readFileSync(path.recipes, 'utf8')),
+      lists: JSON.parse(readFileSync(join(__dirname, 'database/recipes/lists.json'), 'utf8'))
    },
    window: action => {
       ipcRenderer.send('window', action);
--- src/storage.js
+++ src/storage.js
@@ -3,18 +3,41 @@ const { join } = require('path');
 
 let database = join(__dirname, 'database');
 
-let path = {
-   recipes: join(database, 'recipes.json'),
+// Change list new in neverdone if been 1 month from add
+let path = join(database, 'recipes/main.json');
+
+let recipes = JSON.parse(readFileSync(path, 'utf-8'));
+
+for (let recipe in recipes) {
+   if (recipes[recipe].date && recipes[recipe].list) {
+      if ((new Date() - new Date(recipes[recipe].date.split('/').reverse().join('-'))) > 2629800000 && recipes[recipe].list === 'new') recipes[recipe].list = 'neverdone';
+   }
 }
 
-let recipes = JSON.parse(readFileSync(path.recipes, 'utf8'));
+writeFileSync(path, JSON.stringify(recipes, null, 3), 'utf8');
 
-writeFileSync(join(database, 'backup.json'), JSON.stringify(recipes), 'utf8');
+// Backup
+path = join(database, 'recipes/backup.json');
 
-recipes.forEach(recipe => {
-   if (recipe.date) {
-      if ((new Date() - new Date(recipe.date.split('/').reverse().join('-'))) > 2629800000 && recipe.category === 'new') recipe.category = 'neverdone';
-   }
-});
+let backup = JSON.parse(readFileSync(path, 'utf8'));
 
-writeFileSync(path.recipes, JSON.stringify(recipes, null, 3), 'utf8');
+for (let date in backup) {
+   if ((new Date() - new Date(date.split('/').reverse().join('-'))) > 2629800000) delete backup[date];
+}
+
+backup[new Date().toLocaleDateString('it', {
+   day: '2-digit',
+   month: '2-digit',
+   year: 'numeric',
+})] = recipes;
+
+writeFileSync(path, JSON.stringify(backup, null, 3), 'utf8');
+
+// Info
+path = join(database, 'info.json');
+
+let info = JSON.parse(readFileSync(path, 'utf8'));
+
+info.length = Object.keys(recipes).length;
+
+writeFileSync(path, JSON.stringify(info, null, 3), 'utf8');
